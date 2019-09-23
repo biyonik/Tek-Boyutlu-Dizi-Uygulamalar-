7b1a65a880ef52cc6633e7e825343610057d8a2e
biyonik <ahmet.altun60@gmail.com>
2019-09-23 18:27:17 +0300

    Uygulama_8 tamamlandı

---
+++ Uygulama_8/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Uygulama_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPopulate = new System.Windows.Forms.Button();
+            this.btnArithmeticAverage = new System.Windows.Forms.Button();
+            this.btnHarmonicAverage = new System.Windows.Forms.Button();
+            this.btnGeometricAverage = new System.Windows.Forms.Button();
+            this.lstBaseArray = new System.Windows.Forms.ListBox();
+            this.lblTotalSumOfArrayItems = new System.Windows.Forms.Label();
+            this.lblTotalCountOfArray = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPopulate
+            // 
+            this.btnPopulate.Location = new System.Drawing.Point(12, 386);
+            this.btnPopulate.Name = "btnPopulate";
+            this.btnPopulate.Size = new System.Drawing.Size(186, 43);
+            this.btnPopulate.TabIndex = 1;
+            this.btnPopulate.Text = "Doldur";
+            this.btnPopulate.UseVisualStyleBackColor = true;
+            this.btnPopulate.Click += new System.EventHandler(this.BtnPopulate_Click);
+            // 
+            // btnArithmeticAverage
+            // 
+            this.btnArithmeticAverage.Location = new System.Drawing.Point(204, 12);
+            this.btnArithmeticAverage.Name = "btnArithmeticAverage";
+            this.btnArithmeticAverage.Size = new System.Drawing.Size(188, 41);
+            this.btnArithmeticAverage.TabIndex = 2;
+            this.btnArithmeticAverage.Text = "Aritmetik Ortalama Hesapla";
+            this.btnArithmeticAverage.UseVisualStyleBackColor = true;
+            this.btnArithmeticAverage.Click += new System.EventHandler(this.BtnArithmeticAverage_Click);
+            // 
+            // btnHarmonicAverage
+            // 
+            this.btnHarmonicAverage.Location = new System.Drawing.Point(204, 59);
+            this.btnHarmonicAverage.Name = "btnHarmonicAverage";
+            this.btnHarmonicAverage.Size = new System.Drawing.Size(188, 41);
+            this.btnHarmonicAverage.TabIndex = 2;
+            this.btnHarmonicAverage.Text = "Harmonik Ortalama Hesapla";
+            this.btnHarmonicAverage.UseVisualStyleBackColor = true;
+            this.btnHarmonicAverage.Click += new System.EventHandler(this.BtnHarmonicAverage_Click);
+            // 
+            // btnGeometricAverage
+            // 
+            this.btnGeometricAverage.Location = new System.Drawing.Point(204, 106);
+            this.btnGeometricAverage.Name = "btnGeometricAverage";
+            this.btnGeometricAverage.Size = new System.Drawing.Size(188, 41);
+            this.btnGeometricAverage.TabIndex = 2;
+            this.btnGeometricAverage.Text = "Geomterik Ortalama Hesapla";
+            this.btnGeometricAverage.UseVisualStyleBackColor = true;
+            this.btnGeometricAverage.Click += new System.EventHandler(this.BtnGeometricAverage_Click);
+            // 
+            // lstBaseArray
+            // 
+            this.lstBaseArray.FormattingEnabled = true;
+            this.lstBaseArray.Location = new System.Drawing.Point(12, 12);
+            this.lstBaseArray.Name = "lstBaseArray";
+            this.lstBaseArray.Size = new System.Drawing.Size(186, 368);
+            this.lstBaseArray.TabIndex = 3;
+            // 
+            // lblTotalSumOfArrayItems
+            // 
+            this.lblTotalSumOfArrayItems.AutoSize = true;
+            this.lblTotalSumOfArrayItems.Location = new System.Drawing.Point(204, 170);
+            this.lblTotalSumOfArrayItems.Name = "lblTotalSumOfArrayItems";
+            this.lblTotalSumOfArrayItems.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalSumOfArrayItems.TabIndex = 4;
+            // 
+            // lblTotalCountOfArray
+            // 
+            this.lblTotalCountOfArray.AutoSize = true;
+            this.lblTotalCountOfArray.Location = new System.Drawing.Point(204, 183);
+            this.lblTotalCountOfArray.Name = "lblTotalCountOfArray";
+            this.lblTotalCountOfArray.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalCountOfArray.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.lblTotalCountOfArray);
+            this.Controls.Add(this.lblTotalSumOfArrayItems);
+            this.Controls.Add(this.lstBaseArray);
+            this.Controls.Add(this.btnGeometricAverage);
+            this.Controls.Add(this.btnHarmonicAverage);
+            this.Controls.Add(this.btnArithmeticAverage);
+            this.Controls.Add(this.btnPopulate);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dizi Elemanlarının Aritmetik, Geometrik, Harmonik Ortalamasını";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnPopulate;
+        private System.Windows.Forms.Button btnArithmeticAverage;
+        private System.Windows.Forms.Button btnHarmonicAverage;
+        private System.Windows.Forms.Button btnGeometricAverage;
+        private System.Windows.Forms.ListBox lstBaseArray;
+        private System.Windows.Forms.Label lblTotalSumOfArrayItems;
+        private System.Windows.Forms.Label lblTotalCountOfArray;
+    }
+}
+
---
+++ Uygulama_8/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_8
+{
+    
+    public partial class Form1 : Form
+    {
+        private const int ARRAY_SIZE = 50;
+        private int[] intArray;
+        private enum CalculateType
+        {
+            Arithmetic = 1,
+            Harmonic = 2,
+            Geometric = 3
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            intArray = new int[ARRAY_SIZE];
+        }
+
+        private void populate(int[] paramArray)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < paramArray.Length; i++)
+            {
+                int item = rnd.Next(1, 99);
+                paramArray[i] = item;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int arithmeticAverage(int[] paramArr)
+        {
+            int sum = 0;
+            int count = paramArr.Length;
+            for (int i = 0; i < paramArr.Length; i++)
+            {
+                sum += paramArr[i];
+            }
+            return sum / count;
+        }
+
+        private double geometricAverage(int[] paramArr)
+        {
+            int multiplication = 0;
+            int count = paramArr.Length;
+            for (int i = 0; i < paramArr.Length; i++)
+            {
+                multiplication *= paramArr[i];
+            }
+            return Math.Pow(multiplication, 1 / count);
+        }
+
+        private double harmonicAverage(int[] paramArr)
+        {
+            double harmonic = 0;
+            int count = paramArr.Length;
+            for (int i = 0; i < paramArr.Length; i++)
+            {
+                harmonic += 1/paramArr[i];
+            }
+            return paramArr.Length / harmonic;
+        }
+
+        private double calculateAverage(int[] paramArray, CalculateType calculateType)
+        {
+            double result = 0;
+            if(calculateType == CalculateType.Arithmetic)
+            {
+                result = arithmeticAverage(paramArr: paramArray);
+            } else if(calculateType == CalculateType.Geometric)
+            {
+                result = geometricAverage(paramArr: paramArray);
+            } else if(calculateType == CalculateType.Harmonic)
+            {
+                result = harmonicAverage(paramArr: paramArray);
+            }
+            return result;
+        }
+
+        private void BtnPopulate_Click(object sender, EventArgs e)
+        {
+            lstBaseArray.Items.Clear();
+            populate(intArray);
+            int sum = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                lstBaseArray.Items.Add(String.Format("intArray[{0}] = {1}", i, intArray[i]));
+                sum += intArray[i];
+            }
+            lblTotalSumOfArrayItems.Text = $"Dizi Elemanlarının Toplamı: {sum}";
+            lblTotalCountOfArray.Text = $"Dizinin Eleman Sayısı: {intArray.Length}";
+        }
+
+        private void BtnArithmeticAverage_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(String.Format("Dizinin aritmetik ortalaması: {0:0.00}", calculateAverage(intArray, CalculateType.Arithmetic).ToString()));
+        }
+
+        private void BtnHarmonicAverage_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(String.Format("Dizinin harmonik ortalaması: {0:0.00}", calculateAverage(intArray, CalculateType.Harmonic).ToString()));
+        }
+
+        private void BtnGeometricAverage_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(String.Format("Dizinin geometrik ortalaması: {0:0.00}", calculateAverage(intArray, CalculateType.Geometric).ToString()));
+        }
+    }
+}
